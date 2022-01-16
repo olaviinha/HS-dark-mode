a14535669e16f203b02e8e8ff772d0e22851bbf5
O. Inha <50331907+olaviinha@users.noreply.github.com>
2022-01-16 14:44:03 +0200

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -1,5 +1,5 @@
-var invert = setInterval(function(){
-	document.querySelector('body').style='filter: invert(1) hue-rotate(180deg) brightness(.85)';
-	document.querySelectorAll('img, .bg-header-main, iframe').forEach(function(revert) { revert.style='filter: invert(1) hue-rotate(180deg) brightness(1)' });
-	document.querySelectorAll('iframe').forEach(function(dim) { dim.style='brightness(.65)' });
+setInterval(function(){
+	document.querySelector('body').style='filter: invert(1) hue-rotate(180deg) brightness(.75)';
+	document.querySelectorAll('img, .bg-header-main').forEach(function(revert) { revert.style='filter: invert(1) hue-rotate(180deg) brightness(1)'; });
+	document.querySelectorAll('iframe, .article-ad-placeholder').forEach(function(remove) { remove.remove(); });
 }, 1000);
