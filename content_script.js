b8ec9ff3a63055d4448a14c632af08d844b7c8c2
O. Inha <50331907+olaviinha@users.noreply.github.com>
2022-01-16 23:35:39 +0200

    Remember user's toggle switch position

--- content_script.js
+++ content_script.js
@@ -1,16 +1,27 @@
 var switch_location = '.bg-header-main .justify-between > .flex.items-center:last-child';
-var dark_mode_switch = '<label class="dark-switch flex justify-between items-center cursor-pointer md:inline-flex md:justify-start"><input type="checkbox" class="sr-only switch__input" id="dark-mode" checked=""><div onclick="javascript:document.querySelector(\'body\').classList.toggle(\'dark\')" class="switch relative inline-block w-50 h-24 rounded-full md:ml-16"><div class="switch__knob absolute h-24 w-24 rounded-full"></div></div></label>';
+var dark_mode_switch = '<label class="dark-switch flex justify-between items-center cursor-pointer md:inline-flex md:justify-start"><input type="checkbox" class="sr-only switch__input" id="dark-mode" checked=""><div onclick="" class="switch theme-switch relative inline-block w-50 h-24 rounded-full md:ml-16"><div class="switch__knob absolute h-24 w-24 rounded-full"></div></div></label>';
 var theme_added = false;
 
+// Remember user's theme setting
+var user_setting = window.localStorage.getItem('theme');
+function changeTheme() {
+	document.querySelector('body').classList.toggle('dark');
+	if(document.querySelector('body').classList.contains('dark')) {
+		window.localStorage.removeItem('theme');
+	} else {
+		window.localStorage.setItem('theme', 'original');
+	}
+}
+
+// Enable dark theme by default
 var enable_theme = setInterval(function(){
-  // Add dark theme
-	if(document.querySelector('body') && !theme_added) {
+	if(!user_setting && document.querySelector('body') && !theme_added) {
 		document.querySelector('body').classList.add('dark');
 		theme_added = true;
 	}
-  // Add switch
 	if (document.readyState === "complete" || document.readyState === "interactive") {
 		document.querySelector(switch_location).insertAdjacentHTML("beforeend", dark_mode_switch);
-		clearInterval(enable_theme); // Done
+		document.querySelector('.theme-switch').onclick = function() { changeTheme(); }
+		clearInterval(enable_theme);
 	}
 }, 20);
