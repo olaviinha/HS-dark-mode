ccfccb2adcef9a32d86d79d17a5e49b10184c19f
O. Inha <50331907+olaviinha@users.noreply.github.com>
2022-01-17 02:24:36 +0200

    Transition

--- content_script.js
+++ content_script.js
@@ -1,13 +1,14 @@
 var switch_location = '.bg-header-main .justify-between > .flex.items-center:last-child';
 var dark_mode_switch = '<label class="dark-switch flex justify-between items-center cursor-pointer md:inline-flex md:justify-start"><input type="checkbox" class="sr-only switch__input" id="dark-mode" checked=""><div onclick="" class="switch theme-switch relative inline-block w-50 h-24 rounded-full md:ml-16"><div class="switch__knob absolute h-24 w-24 rounded-full"></div></div></label>';
 var theme_added = false;
-const addCSS = s => document.head.appendChild(document.createElement("style")).innerHTML = s;
-
-// Remember user's theme setting
 var user_setting = window.localStorage.getItem('theme');
+
+// Toggle theme
 function changeTheme() {
-	document.querySelector('body').classList.toggle('dark');
-	if(document.querySelector('body').classList.contains('dark')) {
+	var b = document.querySelector('body');
+	b.classList.toggle('dark');
+	// Remember user's theme setting
+	if(b.classList.contains('dark')) {
 		window.localStorage.removeItem('theme');
 	} else {
 		window.localStorage.setItem('theme', 'original');
@@ -16,8 +17,9 @@ function changeTheme() {
 
 // Enable dark theme & add switch
 var enable_theme = setInterval(function(){
+	var b = document.querySelector('body');
 	// Enable theme
-	if(document.querySelector('body') && !user_setting && !theme_added) {
+	if(b && !user_setting && !theme_added) {
 		document.querySelector('body').classList.add('dark');
 		theme_added = true;
 	}
@@ -26,6 +28,7 @@ var enable_theme = setInterval(function(){
 		document.querySelector(switch_location).insertAdjacentHTML('beforeend', dark_mode_switch);
 		document.querySelector('#dark-mode').checked=!user_setting;
 		document.querySelector('.theme-switch').onclick = function() { changeTheme(); }
+		b.style.transition='.6s filter ease-in-out';
 		clearInterval(enable_theme);
 	}
 }, 20);
