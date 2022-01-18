135547e80433662dfa509731ae962c6af95623f9
O. Inha <50331907+olaviinha@users.noreply.github.com>
2022-01-18 17:38:54 +0200

    Fix modals, sticky videos, etc.

--- content_script.js
+++ content_script.js
@@ -1,10 +1,11 @@
 var switch_location = '.bg-header-main .justify-between > .flex.items-center:last-child';
 var dark_mode_switch = '<label class="dark-switch flex justify-between items-center cursor-pointer md:inline-flex md:justify-start"><input type="checkbox" class="sr-only switch__input" id="dark-mode" checked=""><div onclick="" class="switch theme-switch relative inline-block w-50 h-24 rounded-full md:ml-16"><div class="switch__knob absolute h-24 w-24 rounded-full"></div></div></label>';
 var user_setting = window.localStorage.getItem('theme');
+var el = 'html';
 
 // Toggle theme
 function changeTheme() {
-	var b = document.querySelector('body');
+	var b = document.querySelector(el);
 	b.classList.toggle('dark');
 	// Remember theme
 	if(b.classList.contains('dark')) {
@@ -17,14 +18,14 @@ function changeTheme() {
 // Enable dark theme on pageload
 if(!user_setting){
 	var enable_theme = setInterval(function(){
-		if(document.querySelector('body')) {
-			document.querySelector('body').classList.add('dark');
+		if(document.querySelector(el)) {
+			document.querySelector(el).classList.add('dark');
 			clearInterval(enable_theme);
 		}
 	}, 20);
 }
 
-// Inject toggle switch when page loaded
+// Inject theme toggle switch once page loaded
 document.onreadystatechange = function () {
 	if (document.readyState === 'complete' || document.readyState === 'interactive') {
 		document.querySelector(switch_location).insertAdjacentHTML('beforeend', dark_mode_switch);
@@ -32,4 +33,3 @@ document.onreadystatechange = function () {
 		document.querySelector('.theme-switch').onclick = function() { changeTheme(); }
 	}
 }
-
