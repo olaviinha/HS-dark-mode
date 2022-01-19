5b4febf4509a496c2a73cd08dd64086f1e997956
O. Inha <50331907+olaviinha@users.noreply.github.com>
2022-01-19 03:02:26 +0200

    Optimize

--- content_script.js
+++ content_script.js
@@ -1,14 +1,13 @@
-var switch_location = '.bg-header-main .justify-between > .flex.items-center:last-child';
-var dark_mode_switch = '<label class="dark-switch flex justify-between items-center cursor-pointer md:inline-flex md:justify-start"><input type="checkbox" class="sr-only switch__input" id="dark-mode" checked=""><div onclick="" class="switch theme-switch relative inline-block w-50 h-24 rounded-full md:ml-16"><div class="switch__knob absolute h-24 w-24 rounded-full"></div></div></label>';
-var dark_mode_off = window.localStorage.getItem('theme');
-var el = 'html';
+const switch_location = '.bg-header-main .justify-between > .flex.items-center:last-child';
+const dark_mode_switch = '<label class="dark-switch flex justify-between items-center cursor-pointer md:inline-flex md:justify-start"><input type="checkbox" class="sr-only switch__input" id="dark-mode" checked=""><div onclick="" class="switch theme-switch relative inline-block w-50 h-24 rounded-full md:ml-16"><div class="switch__knob absolute h-24 w-24 rounded-full"></div></div></label>';
+const html = document.querySelector('html');
+let dark_mode_off = window.localStorage.getItem('theme');
 
 // Toggle theme
 function changeTheme() {
-	var b = document.querySelector(el);
-	b.classList.toggle('dark');
+	html.classList.toggle('dark');
 	// Remember theme
-	if(b.classList.contains('dark')) {
+	if(html.classList.contains('dark')) {
 		window.localStorage.removeItem('theme');
 	} else {
 		window.localStorage.setItem('theme', 'original');
@@ -16,7 +15,7 @@ function changeTheme() {
 }
 
 // Enable dark theme on pageload
-if(!dark_mode_off) document.querySelector(el).classList.add('dark');
+if(!dark_mode_off) html.classList.add('dark');
 
 // Inject theme toggle switch once page loaded
 document.onreadystatechange = function () {
